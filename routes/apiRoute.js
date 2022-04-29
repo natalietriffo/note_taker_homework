02f1958e0d8c3febb477e4741951388772f8295b
Natalie Triffo <nattriffo@gmail.com>
2022-04-28 22:34:12 -0500

    edits to server file to include middleware

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,25 +1,61 @@
-var path = require("path");
 var dbNote = require("../db/db.json");
 const { v4: uuidv4 } = require("uuid");
 const router = require('express').Router();
 const fs = require("fs");
 
 
-    // API get request for note data
-router.get("/api/notes",function(req,res){
-    res.json(dbNote)
+
+
+
+// API get request for note data
+router.get("/", (req, res) => {
+    res.json(JSON.parse(fs.readFileSync(dbNote, "utf8")))
 })
 
-    // API post request to add note Data
-    router.get("/api/notes",function(req,res){
-        // res.json(dbNote)
-        req.body.id = uuid4()
-        dbNote.push(req.body)
-        fs.writeFile("../db/db.json",JSON.stringify(dbNote),function(err){
-            if(err){
-                throw err
+// API post request to add note Data
+router.post("/", (req, res) => {
+    // res.json(dbNote)
+
+    //what the note consists of 
+    const { title, text, id } = req.body
+
+    //if title and text are present 
+    // then post the new note
+    if (title && text) {
+
+        //creating new obj of what makes up the new note
+        const newNote = {
+            title, text, id: uuidv4()
+        }
+        console.log(newNote)
+        // we recieve an onj back and we need to stringify it 
+        var notebook = JSON.stringify(newNote)
+
+        //reading contents of db.json file
+        fs.readFile(dbNote, "utf8", (err, notebook) => {
+            if (err) {
+                console.log("err on read file" + err)
+            }
+            console.log("success")
+        })
+
+        //parsing the note book var to the note collection variable 
+        var noteCollection = JSON.parse(notebook);
+        noteCollection.push(newNote);
+
+        //writing to db.json file
+        fs.writeFile(dbNote, JSON.stringify(noteCollection), function (err) {
+            if (err) {
+                console.log("err on write file" + err)
             }
             res.json("Note has been saved.")
         })
-    })
+
+        res.json(noteCollection)
+    }
+
+
+
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,18 @@ const express = require ("express")
 const HTMLroutes = require ("./routes/htmlRoute") 
 const APIroutes = require ("./routes/apiRoute")
 
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+
 
 //Middleware
-app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
 app.use(express.static('public'));
-app.use('/', HTMLroutes);
 app.use("/api/notes", APIroutes);
+app.use('/', HTMLroutes);
+
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
