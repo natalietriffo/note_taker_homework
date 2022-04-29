93012897eb13367a1da983d29fc1007a37d228ab
Natalie Triffo <nattriffo@gmail.com>
2022-04-28 21:48:13 -0500

    adjusting html routes

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -1,17 +1,20 @@
 var path = require("path");
-module.exports = function(app){
+var router = require("express").Router();
+
+
+
     // This will get the home route.
-    app.get("/",function(req,res){
+    router.get("/",function(req,res){
         res.sendFile(path.join(__dirname,"../public/index.html"))
     })
 
     // This will get the notes page.
-    app.get("/notes",function(req,res){
+    router.get("/notes",function(req,res){
         res.sendFile(path.join(__dirname,"../public/notes.html"))
     })
 
     // This is the wildcard route.
-    app.get("*",function(req,res){
+    router.get("*",function(req,res){
         res.sendFile(path.join(__dirname,"../public/index.html"))
     })
-}
+module.exports = router;
